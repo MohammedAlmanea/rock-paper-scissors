96b4d90bd70cea314193bbe180d76aa55bc3ad11
MohammedAlmanea <MohammedAlmanea@outlook.com>
2023-01-17 20:27:22 +0300

    Added game() that plays 5 rounds of rock paper scissors and modified playRound to show what the players choose at each round

--- script.js
+++ script.js
@@ -7,13 +7,24 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toUpperCase();
 
-  if (playerSelection === computerSelection) return 'DRAW!!';
+  if (playerSelection === computerSelection) return `DRAW!! You Played ${playerSelection} and the Computer played ${computerSelection}` ;
 
   if (
     (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
     (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
     (computerSelection === 'PAPER' && playerSelection === 'ROCK')
   )
-    return 'Computer wins';
-  return 'YOU WIN!!';
+    return `Computer wins You Played ${playerSelection} and the Computer played ${computerSelection}`;
+  return `YOU WIN!! You Played ${playerSelection} and the Computer played ${computerSelection}`;
 }
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playerSelection = prompt(`Rock, Paper, Scissors? ${i}/5`).toUpperCase();
+    computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, getComputerChoice()));
+  }
+}
+
+
+game();
