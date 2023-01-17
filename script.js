ba799a6e3e768f03b0cfd4cd3217b58a9aa50b9b
MohammedAlmanea <MohammedAlmanea@outlook.com>
2023-01-17 20:08:02 +0300

    Created playRound function that plays a round on rock paper scissors

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let choices = ['ROCK','PAPER','SCISSORS'];
+let choices = ['ROCK', 'PAPER', 'SCISSORS'];
 
 function getComputerChoice() {
-    return choices[Math.floor(Math.random()*choices.length)]
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toUpperCase();
+
+  if (playerSelection === computerSelection) return 'DRAW!!';
+
+  if (
+    (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
+    (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
+    (computerSelection === 'PAPER' && playerSelection === 'ROCK')
+  )
+    return 'Computer wins';
+  return 'YOU WIN!!';
 }
